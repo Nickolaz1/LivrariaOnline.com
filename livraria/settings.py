98a4fadb31335b519db2200449adce72c1df8fda
Nickolas Alexandre <nickolaz.0603@gmail.com>
2022-11-08 06:42:03 -0300

    primeira view

--- livraria/settings.py
+++ livraria/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
