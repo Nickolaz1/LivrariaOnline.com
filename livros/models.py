82ff668e9f9adb0a4d437191abe20fe4b48761a8
Nickolas Alexandre <nickolaz.0603@gmail.com>
2022-11-09 07:02:29 -0300

    istagem dos primeiros livros

--- livros/models.py
+++ livros/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=30)
+    valor = models.DecimalField(decimal_places=2, max_digits=6)
+    descricao = models.TextField()
+    capa = models.CharField(max_length=200)
+    insert = models.DateTimeField()
+    update = models.DateTimeField()
+    
--- livros/views.py
+++ livros/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Livro
 
 def index(request):
     return render(request, 'livros/index.html', {'nome': 'Nickolas'})
+
+def listar(request):
+    livros = Livro.objects.order_by('id')
+    return render(request, 'livros/lista.html', {'livros': livros})
