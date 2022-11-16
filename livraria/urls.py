65f56c3f84f7d35912a846499c76ac302318cddb
Nickolas Alexandre <nickolaz.0603@gmail.com>
2022-11-15 21:19:42 -0300

    modificando urls

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from livros import views as livros
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', livros.index, name='index'),
     path('livros/', include('livros.urls'))
 ]
