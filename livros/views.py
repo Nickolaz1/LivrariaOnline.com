8dc5bbc00f6cea7c6dcf6beab21b79542401c437
Nickolas Alexandre <nickolaz.0603@gmail.com>
2022-11-16 22:27:34 -0300

    adicionando livros em promoção na home

--- livros/views.py
+++ livros/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Livro
 
 def index(request):
-    return render(request, 'livros/index.html', {'nome': 'Nickolas'})
+    livros = Livro.objects.order_by('id')
+    return render(request, 'livros/index.html', {'livros': livros})
 
 def listar(request):
     livros = Livro.objects.order_by('id')
